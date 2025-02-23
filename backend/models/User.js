093848f09d33619beb617033dd434209a17d6ec3
srimathy18 <logamathy18@gmail.com>
2025-02-23 10:55:00 -0500

    Updated project structure, removed unnecessary files

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  fullName: String,
-  email: { type: String, unique: true },
-  password: String,
-});
-
-module.exports = mongoose.model("User", UserSchema);
